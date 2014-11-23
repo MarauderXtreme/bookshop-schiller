45cf165010994fb66a1f49e960688a2e8c3e4ea4
Christoph Kepler <christoph.kepler@arts.aero>
2014-11-23 11:49:27 +0100

    Login and User prototype

--- src/main/java/bookshop/model/User.java
+++ src/main/java/bookshop/model/User.java
@@ -12,13 +12,16 @@ public class User {
 	@GeneratedValue
 	private long id;
 	
+	private String email;
+	
 	private String address;
 	
 	@OneToOne
 	private UserAccount userAccount;
 	
-	public User(UserAccount userAccount, String address) {
+	public User(UserAccount userAccount, String email, String address) {
 		this.userAccount = userAccount;
+		this.email = email;
 		this.address = address;
 	}
 	
@@ -29,9 +32,21 @@ public class User {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	
+	public String getEMail() {
+		return email;
+	}
+	
+	public void setEMail(String email) {
+		this.email = email;
+	}
 
 	public UserAccount getUserAccount() {
 		return userAccount;
+	}
+	
+	public long getID() {
+		return id;
 	}
 
 }
