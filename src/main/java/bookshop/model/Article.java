5351de79802d45c26c7a256bfd8cc44f5770c162
Philipp W <waackphilipp@gmail.com>
2014-11-24 14:35:58 +0100

    added article classes

--- src/main/java/bookshop/model/Article.java
+++ src/main/java/bookshop/model/Article.java
@@ -1,14 +1,40 @@
 package bookshop.model;
 
-public class Article {
+import org.salespointframework.catalog.Product;
 
-	private String beschreibung;
-	private String title;
-	private int id;
-	
-	public Article(String beschreibung, String title, int id){
-		this.beschreibung = beschreibung;
-		this.title = title;
-		this.id = id;
+
+
+	public abstract class Article extends Product{
+		
+		private String beschreibung;
+		private String title;
+		private int id;
+		
+		//!!! - Kategorie manuell in der Main neben Artikel definieren 
+		//oder im Artikel direkt hinzufügen im Kostruktor
+		private Category category;
+		
+		public Article(String beschreibung, String title, int id){
+			this.beschreibung = beschreibung;
+			this.title = title;
+			this.id = id;
+		}
+		
+		public String getTitle(){
+			return title;
+		}
+		
+		public String getBeschreibung(){
+			return beschreibung;
+		}
+		
+		public void setCategory(Category category){
+			this.category = category;
+		}
+		
+		public Category getCategory(){
+			return this.category;
+		}
+
 	}
-}
+
