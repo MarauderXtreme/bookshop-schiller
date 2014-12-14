f7de7ebbd204772b64a5925dc1ef113d8b946d0c
Philipp W <waackphilipp@gmail.com>
2014-12-14 19:00:32 +0100

    Added javadoc text to article, articlemanagement, categorymanagement, categorycontroller, articlecontroller

--- src/main/java/bookshop/model/ArticleManagement.java
+++ src/main/java/bookshop/model/ArticleManagement.java
@@ -19,18 +19,53 @@ import bookshop.model.Article.ArticleId;
 
 public interface ArticleManagement extends Catalog<Article> {
 
+	/**
+	 * Iterates a catalog of articles
+	 * @param type
+	 * @return a List of all existing articles of a specific article type
+	 */
 	Iterable<Article> findByType(ArticleId type);
 		
+	/**
+	 * Iterates a catalog of articles
+	 * @param publisher
+	 * @return a List of all existing articles of a specific publisher
+	 */
 	Iterable<Article> findByPublisher(String publisher);
 	
+	/**
+	 * Iterates a catalog of articles
+	 * @param id
+	 * @return a List of all existing articles of a specific product number
+	 */
 	Iterable<Article> findById(String id);
 	
+	/**
+	 * Iterates a catalog of articles
+	 * @param author
+	 * @return a List of all existing articles of a specific author
+	 */
 	Iterable<Article> findByAuthor(String author);
 	
+	/**
+	 * Iterates a catalog of articles
+	 * @param interpret
+	 * @return a List of all existing articles of a specific interpret
+	 */
 	Iterable<Article> findByInterpret(String interpret);
 	
+	/**
+	 * Iterates a catalog of articles
+	 * @param director
+	 * @return a List of all existing articles of a specific director
+	 */
 	Iterable<Article> findByDirector(String director);
 	
+	/**
+	 * Iterates a catalog of articles
+	 * @param category
+	 * @return a List of all existing articles of a specific category
+	 */
 	@Query(value="select p from #{#entityName} p where ?1 member of p.categories")
 	Iterable<Article> findByCategory(String category);
 }
--- src/main/java/bookshop/model/validation/ArticleForm.java
+++ src/main/java/bookshop/model/validation/ArticleForm.java
@@ -11,21 +11,18 @@ import bookshop.model.Article.ArticleId;
 
 public class ArticleForm {
 	
-	//All variables must be not empty
-	
-	
+	//All variables must be not empty	
 	//Darf nur positive Zahl sein
 	//double price;
-	
 	//Was für eine Nummer soll es sein?! Nur für Bücher oder Allg. ID
-	//String ISBN;
-	
+	//String ISBN;	
 	//Symbole "." Sufix(jpg,png,...)
-	//String image;
-	
+	//String image;	
 	//Darf nicht negativ sein
 	//Resultierender Amount darf nicht negativ sein
 	//Quantity amount;
+	
+	
 	
 	@NotEmpty(message = "{ArticleForm.name.NotEmpty}")
 	private String name;
@@ -69,26 +66,47 @@ public class ArticleForm {
 	
 	//Setter and Getter
 	
+	/**
+	 * @return the given article title from the article form
+	 */
 	public String getName(){
 		return name;
 	}
 	
+	/**
+	 * Sets the title of the article form to the given value.
+	 * @param title
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * @return the given description from the article form
+	 */
 	public String getBeschreibung(){
 		return beschreibung;
 	}
 	
+	/**
+	 * Sets the description of the article form to the given value.
+	 * @param description
+	 */
 	public void setBeschreibung(String beschreibung){
 		this.beschreibung = beschreibung;
 	}
 	
+	/**
+	 * @return the given publisher name from the article form
+	 */
 	public String getPublisher(){
 		return publisher;
 	}
 	
+	/**
+	 * Sets the publisher of the article form to the given value.
+	 * @param publisher
+	 */
 	public void setPublisher(String publisher){
 		this.publisher = publisher;
 	}
@@ -103,54 +121,96 @@ public class ArticleForm {
 	}
 	*/
 	
+	/**
+	 * @return the given Productnumber from the article form
+	 */
 	public String getId(){
 		return id;
 	}
 	
+	/**
+	 * Sets the Productnumber of the article form to the given value.
+	 * @param Productnumber
+	 */
 	public void setId(String id){
 		this.id = id;
 	}
 	
 	//Spezifische Variablen
 	
+	/**
+	 * @return the given author name from the article form
+	 */
 	public String getAuthor(){
 		return author;
 	}
 	
+	/**
+	 * Sets the author of the article form to the given value.
+	 * @param author
+	 */
 	public void setAuthor(String author){
 		this.author = author;
 	}
 	
+	/**
+	 * @return the given user interpret name the article form
+	 */
 	public String getInterpret(){
 		return interpret;
 	}
 	
+	/**
+	 * Sets the interpret of the article form to the given value.
+	 * @param interpret
+	 */
 	public void setInterpret(String interpret){
 		this.interpret = interpret;
 	}
 	
+	/**
+	 * @return the given director name from the article form
+	 */
 	public String getDirector(){
 		return director;
 	}
 	
+	/**
+	 * Sets the director of the article form to the given value.
+	 * @param director
+	 */
 	public void setDirector(String director){
 		this.director = director;
 	}
 	
+	/**
+	 * @return the given filename from the article form
+	 */
 	public String getImage(){
 		return image;
 	}
 	
+	/**
+	 * Sets the image of the article form to the given value.
+	 * @param image
+	 */
 	public void setImage(String image){
 		this.image = image;
 	}
 	
+	/**
+	 * @return the given amount of article units from the article form
+	 */
 	public int getAmount(){
 		return amount;
 	}
 	
+	/**
+	 * Sets the amount of the article form to the given value.
+	 * @param amount
+	 */
 	public void setAmount(int amount){
 		this.amount = amount;
 	}
-
+	
 }
