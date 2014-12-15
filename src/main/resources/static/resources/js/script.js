f1a44a5d8fdbbb0e9b96775f6f7747e1ce7d5886
Christoph Kepler <christoph.kepler@arts.aero>
2014-12-15 11:24:22 +0100

    Redesign of Article Editing

--- src/main/resources/static/resources/js/script.js
+++ src/main/resources/static/resources/js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 			patterns: {
 				custom_password: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_\W]).{8,32})/,
 				plz: /^(\d){5}$/,
-				custom_price: /^EUR\s[1-9]{1}[0-9]*\.[0-9]{2}$/
+				custom_price: /^EUR\s[1-9]{1}[0-9]*\.[0-9]{2}$/,
+				isbn13: /^[0-9]{13}$/
 			}
 		}
 	});
