8f5f8f66f89c54b60a801623e0f6b575b6077000
Bartmen <hiho23@live.de>
2014-12-15 01:20:34 +0100

    Implemented statistics

--- src/main/java/bookshop/controller/OrderController.java
+++ src/main/java/bookshop/controller/OrderController.java
@@ -1,12 +1,20 @@
 package bookshop.controller;
 
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpSession;
 
+import org.joda.money.Money;
+import org.salespointframework.catalog.Product;
 import org.salespointframework.inventory.Inventory;
 import org.salespointframework.inventory.InventoryItem;
 import org.salespointframework.order.Order;
+import org.salespointframework.order.OrderLine;
 import org.salespointframework.order.OrderManager;
 import org.salespointframework.order.OrderStatus;
+import org.salespointframework.time.BusinessTime;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -19,17 +27,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class OrderController {
 	private final OrderManager<Order> orderManager;
 	private final Inventory<InventoryItem> inventory;
+	private final BusinessTime date;
 	
 	@Autowired
-	public OrderController(OrderManager<Order> orderManager, Inventory<InventoryItem> inventory){
+	public OrderController(OrderManager<Order> orderManager, Inventory<InventoryItem> inventory, BusinessTime date){
 		this.orderManager = orderManager;
 		this.inventory = inventory;
+		this.date = date;
 	}
 	
 	@PreAuthorize("hasRole('ROLE_BOSS') || hasRole('ROLE_ADMIN')")
 	@RequestMapping("/admin/orders")
 	public String orders(HttpSession session, ModelMap modelMap){
 		modelMap.addAttribute("ordersCompleted", orderManager.find(OrderStatus.COMPLETED));
+		
 		return "/orders";
 	}
 	/*
@@ -39,10 +50,44 @@ public class OrderController {
 		return "redirect:/orders";
 	}*/
 	
-	@RequestMapping("/admin/statistics")
-	public String statistics(HttpSession session, ModelMap modelMap){
+	@RequestMapping("/admin/stock")
+	public String stock(HttpSession session, ModelMap modelMap){
 		modelMap.addAttribute("stock", inventory.findAll());
 		return "/stock";
 	}
 	
+	@RequestMapping("/admin/statictics")
+	public String statistic(HttpSession session, ModelMap modelMap, @LoggedIn UserAccount userAccount){
+		//LocalDateTime date;
+		//date.getTime();
+		LocalDateTime time = date.getTime();
+		time.minusDays(7);		
+		//modelMap.addAttribute("ordersLastWeek", orderManager.find(date.getTime(), time));
+		Order ordersLastWeek = new Order(userAccount);
+		
+		/*
+		for(Order order : orderManager.find(date.getTime(), time)){	
+			for(OrderLine orderLine : order.getOrderLines()){
+				ordersLastWeek.add(orderLine);
+			}
+		}	
+		
+		modelMap.addAttribute("ordersLastWeek", ordersLastWeek.getOrderLines());
+		*/
+		for(Order order : orderManager.find(OrderStatus.COMPLETED)){	
+			for(OrderLine orderLine : order.getOrderLines()){
+				ordersLastWeek.add(orderLine);
+			}
+		}	
+		
+		modelMap.addAttribute("ordersLastWeek", ordersLastWeek.getOrderLines());
+		
+		return "/statistics";
+	}
+	
+	
+	public String getMyOrders(){
+		
+		return "";
+	}
 }
---
+++ src/main/java/bookshop/model/Statistic.java
@@ -0,0 +1,19 @@
+package bookshop.model;
+
+import java.time.LocalDateTime;
+
+public class Statistic {
+	
+	private int gesnumber;
+	private LocalDateTime time;
+	
+	public Statistic(){
+		
+	}
+	
+	
+	
+	public void recalculate(int number){
+		time.getDayOfMonth();
+	}
+}
