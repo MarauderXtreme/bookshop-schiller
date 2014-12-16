1a9420cafd83af3374bcee274c145a358669e210
Christoph Kepler <christoph.kepler@arts.aero>
2014-12-16 20:43:33 +0100

    Added Dummy Implementation of Room Management

---
+++ src/main/java/bookshop/model/validation/RoomForm.java
@@ -0,0 +1,32 @@
+package bookshop.model.validation;
+
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class RoomForm {
+	
+	@NotEmpty(message = "{RoomFrom.roomNumber.NotEmpty}")
+	private String roomNumber;
+	
+	@NotEmpty(message = "{RoomFrom.roomname.NotEmpty}")
+	@Pattern(regexp="([A-Z]{1}.)", message ="{RoomForm.roomName.capitalbeginning}")
+	private String roomName;
+	
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public void setRoomNumber(String number) {
+		this.roomNumber = number;
+	}
+	
+	public String getRoomName() {
+		return roomNumber;
+	}
+	
+	public void setRoomName(String name) {
+		this.roomNumber = name;
+	}
+	
+}
