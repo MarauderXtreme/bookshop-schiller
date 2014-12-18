b591bc1ed5e64eb3c89feedc0d9c7f6466935d0f
Bartmen <hiho23@live.de>
2014-12-18 02:09:21 +0100

    #13 Update PDF

--- src/main/java/bookshop/model/PDFCreator.java
+++ src/main/java/bookshop/model/PDFCreator.java
@@ -9,12 +9,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
+import org.salespointframework.inventory.InventoryItem;
 import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderIdentifier;
 import org.salespointframework.order.OrderLine;
 import org.salespointframework.useraccount.UserAccount;
 
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 
@@ -32,6 +35,7 @@ public class PDFCreator extends HttpServlet {
 	public void pdfCreate(Order order, UserAccount userAccount){
 		Document document = new Document();
 		HttpServletResponse test;
+		InventoryItem item;
 		//String curDir = System.getProperty(")
 	    try {
 	        PdfWriter.getInstance(document,
@@ -40,15 +44,42 @@ public class PDFCreator extends HttpServlet {
 	        document.open();
 	        document.add(new Paragraph("Bestellnummer:"));
 	        document.add(new Paragraph(order.getIdentifier().toString()));
+	        document.add(new Paragraph("Datum:"));
+	        document.add(new Paragraph(order.getDateCreated().toString()));
 	        document.add(new Paragraph("Kunde:"));
 	        document.add(new Paragraph(userAccount.getFirstname()));
 	        document.add(new Paragraph(userAccount.getLastname()));
-	        document.add(new Paragraph("Artikel:"));
+	        //document.add(new Paragraph("Artikel:"));
+	        
+	        PdfPTable table = new PdfPTable(4);
+	        PdfPCell cell1 = new PdfPCell(new Paragraph("Menge"));
+	        PdfPCell cell2 = new PdfPCell(new Paragraph("Artikel"));
+	        PdfPCell cell3 = new PdfPCell(new Paragraph("Preis"));
+	        PdfPCell cell4 = new PdfPCell(new Paragraph("Gesamt"));
+	        
+	        /*
+	        cell1.setBorder(Rectangle.NO_BORDER);
+	        cell2.setBorder(Rectangle.NO_BORDER);
+	        cell3.setBorder(Rectangle.NO_BORDER);
+	        cell4.setBorder(Rectangle.NO_BORDER);
+	        */
+	        
+	        table.addCell(cell1);
+	        table.addCell(cell2);
+	        table.addCell(cell3);
+	        table.addCell(cell4);
 	        
 	        for(OrderLine orderLine : order.getOrderLines()){
-	        	document.add(new Paragraph(orderLine.getProductName()));
-	        	document.add(new Paragraph(orderLine.getQuantity().toString()));
+	        	table.addCell(orderLine.getQuantity().toString());
+	        	table.addCell(orderLine.getProductName());
+	        	table.addCell("test");
+	        	table.addCell(orderLine.getPrice().toString());
 	        }
+	        	        
+	        document.add(table);
+	        document.add(new Paragraph("Gesamtbetrag:"));
+	        document.add(new Paragraph(order.getTotalPrice().toString()));
+
 	        document.close();
 	
 	    } catch (DocumentException e) {
