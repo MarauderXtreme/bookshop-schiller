ad41c9fa4b9538792e547c19100c74572ff8d221
Christoph Kepler <christoph.kepler@arts.aero>
2014-12-19 14:42:33 +0100

    Fixing build errors

--- src/main/java/bookshop/controller/RoomController.java
+++ src/main/java/bookshop/controller/RoomController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.thoughtworks.xstream.mapper.SystemAttributeAliasingMapper;
-
 import bookshop.model.RoomManagement;
 
 @Controller
