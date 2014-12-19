17469cc02ef6f1704400bc8edf26dd9fe3b77ab9
Christoph Kepler <christoph.kepler@arts.aero>
2014-12-19 16:50:48 +0100

    Updated JS Validation [skip ci]

--- src/main/resources/static/resources/js/script.js
+++ src/main/resources/static/resources/js/script.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
 			patterns: {
 				custom_password: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_\W]).{8,32})/,
 				plz: /^(\d){5}$/,
-				custom_price: /^EUR\s[1-9]{1}[0-9]*\.[0-9]{2}$/,
+				custom_price: /^EUR\s[1-9]{1}\d*\.\d{2}$/,
 				isbn13: /^[0-9]{13}$/,
 				caps_start: /^[A-Z](1).*/,
-				custom_date: /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/,
-				custom_time: /^[0-9]{2}:[0-9]{2}$/
+				custom_date: /^(0[1-9]|[12]\d|3[0-1])\.(0[1-9]|1[0-2])\.\d{4}$/,
+				custom_eventdate: /^(0[1-9]|[12]\d|3[0-1])(0[1-9]|1[0-2])\d{4}$/,
+				custom_time: /^(0[1-9]|1\d|2[0-3])([0-5]\d)$/
 			}
 		}
 	});
