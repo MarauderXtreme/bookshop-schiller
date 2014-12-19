5509fa3f10abc5f150450da8db7a892120fa943d
MDuehr <maximiliandh@gmail.com>
2014-12-19 23:05:19 +0100

    Hinweistexte hinzugefuegt, damit gueltige eingaben klar sind

--- src/main/java/bookshop/controller/RoomController.java
+++ src/main/java/bookshop/controller/RoomController.java
@@ -3,6 +3,8 @@ package bookshop.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +16,17 @@ public class RoomController {
 
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value="/admin/room/new", method = RequestMethod.POST)
-	public String add(@RequestParam("rName") String rName, @RequestParam("rNum") String rNum, @RequestParam("rChairs") String chairs)
+	public String add(@RequestParam("rName") String rName, @RequestParam("rNum") String rNum, @RequestParam("rChairs") String chairs, BindingResult result)
 	{
-		RoomManagement.getInstance().addRoom(rName, rNum, chairs);
+		if(!RoomManagement.getInstance().addRoom(rName, rNum, chairs))
+		{
+			result.addError(new ObjectError("Room", "Der eingegebene Raumname oder die eingegebene Raumnummer ist bereits vergeben"));
+		}
+		
+		if(result.hasErrors())
+		{
+			
+		}
 		return "redirect:/admin/room/add";
 	}
 	
