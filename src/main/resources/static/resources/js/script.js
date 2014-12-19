0c2b6881ec0d4888b82d2b8e46a1532698f2c7ed
Christoph Kepler <christoph.kepler@arts.aero>
2014-12-19 17:31:26 +0100

    Custom Price regex Fix for 0.99

--- src/main/resources/static/resources/js/script.js
+++ src/main/resources/static/resources/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 			patterns: {
 				custom_password: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_\W]).{8,32})/,
 				plz: /^(\d){5}$/,
-				custom_price: /^[1-9]{1}\d*\.\d{2}$/,
+				custom_price: /^([1-9]{1}\d*|0)\.\d{2}$/,
 				isbn13: /^[0-9]{13}$/,
 				caps_start: /^[A-Z](1).*/,
 				custom_date: /^(0[1-9]|[12]\d|3[0-1])\.(0[1-9]|1[0-2])\.\d{4}$/,
