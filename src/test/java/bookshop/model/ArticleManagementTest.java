5680e8e09a880a3b54aa2e1aef33c2e506a1c4cf
Philipp W <waackphilipp@gmail.com>
2014-12-21 14:11:06 +0100

    added junit tests for articlemanagement, categorymanagement, category

--- src/test/java/bookshop/model/ArticleManagementTest.java
+++ src/test/java/bookshop/model/ArticleManagementTest.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import bookshop.AbstractIntegrationTests;
 import bookshop.model.Article.ArticleId;
 
-public class ArticleTest extends AbstractIntegrationTests {
+public class ArticleManagementTest extends AbstractIntegrationTests {
 
+	@Autowired 
+	ArticleManagement articleCatalog;
 	@Autowired
 	CategoryManagement categories;
 
@@ -23,14 +25,15 @@ public class ArticleTest extends AbstractIntegrationTests {
 	public void testStringGetters() {
 		
 		Article article = new Article("Testbook", Money.of(EUR, 7.98), "Book for JUnit", "Philo", "0000000101010", ArticleId.BOOK, categories.findById("BOOKRatgeber").get().getCategoryName(), "Flann O'Brien", "01.01.2015", Money.of(EUR, 0.99));
-		article.setArtist("Jambala");
-		//..
 		Category category = new Category("Jupidu", ArticleId.BOOK);
 		article.addCategory(category.getCategoryName());
 		article.removeCategory(categories.findById("BOOKRatgeber").get().getCategoryName());
+		articleCatalog.save(article);
 		
-		assertTrue("Die Methode getType() der Klasse Article liefert einen falschen Wert der Enumeration ArticleId.", article.getType().equals(ArticleId.BOOK));
-		assertEquals("Die Methode getCategoriesList der Klasse Address liefert einen falschen String.", article.getCategoryList(), "[Jupidu]");
+		//...
+
+		assertTrue("Die Methode getType() der Klasse Article liefert einen falschen Wert der Enumeration ArticleId.", articleCatalog.findOne(article.getIdentifier()).get().getType().equals(ArticleId.BOOK));
+		assertEquals("Die Methode getCategoriesList der Klasse Address liefert einen falschen String.", articleCatalog.findOne(article.getIdentifier()).get().getArtist(), "Flann O'Brien");
 	}
 
 }
---
+++ src/test/java/bookshop/model/CategoryTest.java
@@ -0,0 +1,30 @@
+package bookshop.model;
+
+import static org.joda.money.CurrencyUnit.EUR;
+import static org.junit.Assert.*;
+
+import org.joda.money.Money;
+import org.junit.Test;
+import org.salespointframework.useraccount.Role;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import bookshop.AbstractIntegrationTests;
+import bookshop.model.Article.ArticleId;
+
+public class CategoryTest extends AbstractIntegrationTests {
+
+	
+	@Test
+	public void testStringGetters() {
+		
+		Category category = new Category("Jupidu", ArticleId.BOOK);
+		//...
+		
+		
+		assertTrue("Die Methode getType() der Klasse Article liefert einen falschen Wert der Enumeration ArticleId.", category.getType().equals(ArticleId.BOOK));
+		assertEquals("Die Methode getCategoriesList der Klasse Address liefert einen falschen String.", category.getCategoryName(), "Jupidu");
+	}
+
+}
