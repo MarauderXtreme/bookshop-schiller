de68170242c23cb6edd80e34a8bce67680fe5192
MDuehr <maximiliandh@gmail.com>
2014-12-22 14:02:12 +0100

    changed last parameter to have other test-rooms than in "calendarManagementTest"

--- src/test/java/bookshop/model/RoomManagerTest.java
+++ src/test/java/bookshop/model/RoomManagerTest.java
@@ -14,7 +14,7 @@ public class RoomManagerTest extends AbstractIntegrationTests {
 		assertEquals("Ein Raum wird dem RoomManagement hinzugefuegt",true, RoomManagement.getInstance().addRoom("Raum0001", "1010101", "40"));
 		assertEquals("Ein Raum wird dem RoomManagement hinzugefuegt",false, RoomManagement.getInstance().addRoom("Raum0001","112","234"));
 		assertEquals("Ein Raum wird dem RoomManagement hinzugefuegt",false,RoomManagement.getInstance().addRoom("Raum0011","1010101","234"));
-		assertEquals("Ein Raum wird dem RoomManagement hinzugefuegt",true,RoomManagement.getInstance().addRoom("Raum0100","114","24"));
+		assertEquals("Ein Raum wird dem RoomManagement hinzugefuegt",true,RoomManagement.getInstance().addRoom("Raum0100","0110110","24"));
 		
 		assertTrue("Ein Raum kann nicht entfernt werden",RoomManagement.getInstance().removeRoom("Testraum1"));
 		assertEquals("Ein Raum wird entfernt,obwohl es ihn nicht gibt",false,RoomManagement.getInstance().removeRoom("Testraum1"));
