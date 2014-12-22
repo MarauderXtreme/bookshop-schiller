19ea7a3521d05c97ead9b149b6c5fd2064856cec
MDuehr <maximiliandh@gmail.com>
2014-12-22 13:43:37 +0100

    Fixing tests for Travis

--- src/test/java/bookshop/model/CalendarManagementTest.java
+++ src/test/java/bookshop/model/CalendarManagementTest.java
@@ -18,16 +18,16 @@ public class CalendarManagementTest extends AbstractIntegrationTests {
 	@Test
 	public void testCalendarEvents()
 	{
-		RoomManagement.getInstance().addRoom("Testraum1", "111", "40");
-		 RoomManagement.getInstance().addRoom("Testraum2","114","24");
+		RoomManagement.getInstance().addRoom("Raum1", "111", "40");
+		RoomManagement.getInstance().addRoom("Raum2","114","24");
 
 		MyDate testdate = new MyDate("11092014","1110");
 		MyDate testdate2 = new MyDate("10092014","1110");
 		
-		Event testevent1 = new Event("Testevent 1", testdate, RoomManagement.getInstance().getRoom("Testraum1"));
-		Event testevent2 = new Event("Testevent 2", testdate2, RoomManagement.getInstance().getRoom("Testraum2"));
-		Event testevent3 = new Event("Testevent 3", testdate2, RoomManagement.getInstance().getRoom("Testraum2"));
-		Event testevent4 = new Event("Testevent 4", testdate, RoomManagement.getInstance().getRoom("Testraum2"));
+		Event testevent1 = new Event("Testevent 1", testdate, RoomManagement.getInstance().getRoom("Raum1"));
+		Event testevent2 = new Event("Testevent 2", testdate2, RoomManagement.getInstance().getRoom("Raum2"));
+		Event testevent3 = new Event("Testevent 3", testdate2, RoomManagement.getInstance().getRoom("Raum2"));
+		Event testevent4 = new Event("Testevent 4", testdate, RoomManagement.getInstance().getRoom("Raum2"));
 		
 		
 		assertTrue("Die Methode addEvent() des Calendar-objekts hat einen fehler  ", CalendarManagement.getInstance().getCalendar().addEvent(testevent1));
